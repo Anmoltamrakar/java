83e5628a45ed59cffdb95fa88666191af3086852
Anmol Tamrakar <59768896+Anmoltamrakar@users.noreply.github.com>
2021-12-15 00:37:12 +0545

    Lab assignment 4,5,8

---
+++ Labs/Lab1/Qn5/command.java
@@ -0,0 +1,10 @@
+class SumTwoNumber{
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+        int diff =a-b;
+        System.out.println("The sum is" +sum);
+        System.out.println("The diff is" +diff);
+    }
+}
